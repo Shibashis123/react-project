c3432c9928cce28401b4dc77e7c5f12ec108a3e5
Shibashis Bose <shibashisbose1499@gmail.com>
2024-10-22 19:28:02 +0530

    new commit

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
 function App() {
+  const [Counter, setCounter] = useState(15);
+  //let Counter = 15;
+
+  const addValue = () => {
+    // Counter = Counter + 1;
+    console.log("clicked", Counter);
+    setCounter(Counter + 1);
+  };
+  const removeValue = () => {
+    //Counter = Counter - 1;
+    console.log("Clicked", Counter);
+    setCounter(Counter - 1);
+
+    if (Counter > 25) {
+      alert("please provide proper value to remove ");
+    } else {
+      alert("shiba will be successfull");
+    }
+    if (Counter < 15) {
+      alert("shiba is good at coding");
+    } else {
+      alert("shiba will be successfull");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>shiba will be successfull </h1>
+      <h2>one more thing i have to master on this react </h2>
+      <h2>Counter value :{Counter} </h2>
+      <h3>shiba is a good programmer</h3>
+
+      <button id="btn" onClick={addValue}>
+        Add value{Counter}
+      </button>
+      <br />
+      <button id="btn" onClick={removeValue}>
+        remove value{Counter}
+      </button>
+      <p className="footer">
+        shiba is in under pressure but he will be successfull . Jai
+        bajrangbali....
+      </p>
+    </>
   );
 }
 
